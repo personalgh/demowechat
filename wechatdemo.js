b8f876d797ef4aca48b6b14b8aebf2a3339b49fd
M.Air <bjshizimo@163.com>
2021-08-25 01:01:34 +1000

    add dialog box

---
+++ wechatdemo.js
@@ -0,0 +1,9 @@
+let tabs = document.getElementsByClassName("tab");
+let chat_name = document.getElementsByClassName("chatName")[0];
+console.log(chat_name);
+for(let i = 0; i < tabs.length; i++){
+  tabs[i].addEventListener("click", (e)=>{
+    let this_name = e.currentTarget.firstElementChild.nextElementSibling.firstElementChild.innerHTML;
+    chat_name.innerHTML = this_name;
+  })
+}
