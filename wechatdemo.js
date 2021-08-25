33684ce637af862c5a238910cff9f98638b6c1d5
M.Air <bjshizimo@163.com>
2021-08-25 17:34:51 +1000

    add js to send message

--- wechatdemo.js
+++ wechatdemo.js
@@ -14,3 +14,34 @@ for(let i = 0; i < tabs.length; i++){
     prev_tab = this_tab;
   });
 }
+let send_button = document.getElementsByClassName("sendButton")[0];
+let chat_content = document.getElementsByClassName("chatContent")[0];
+let chat_input_box = document.getElementsByClassName("inputBoxDiv")[0].firstElementChild;
+function send_message(){
+  let this_chat = chat_input_box.value;
+  if(!this_chat.length) return;
+  let demo_chat_message = document.getElementsByClassName("myMessageBox")[0].cloneNode(true);
+  demo_chat_message.firstElementChild.innerHTML = this_chat;
+  chat_content.appendChild(demo_chat_message);
+  chat_input_box.value='';
+  chat_content.scrollTop = chat_content.scrollHeight;
+}
+send_button.addEventListener("click", (e)=>{
+  send_message();
+  // let this_chat = chat_input_box.value;
+  // if(!this_chat.length) return;
+  // let demo_chat_message = document.getElementsByClassName("myMessageBox")[0].cloneNode(true);
+  // demo_chat_message.firstElementChild.innerHTML = this_chat;
+  // chat_content.appendChild(demo_chat_message);
+  // chat_input_box.value='';
+  // chat_content.scrollTop = chat_content.scrollHeight;
+});
+let input_area = document.getElementsByClassName("inputArea")[0];
+input_area.onkeydown = function(e){
+  if(e.keyCode === 13){
+    e.preventDefault();
+    e.returnValue = false;
+    send_message();
+    return false;
+  }
+}
